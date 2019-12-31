196bb3cc803c8039a90b5925da17650bbb13f1dc
Richard <richardli125@gmail.com>
2019-12-31 03:32:51 -0500

    added reducer

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
-import allReducers from './reducers';
+import rootReducer from './reducers';
 import { Provider } from 'react-redux';
 
-const store = createStore(allReducers);
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
