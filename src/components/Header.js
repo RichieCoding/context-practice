ae8780e8d3f0c1d9cf745801624d058fe31a3f4f
Richard <richardli125@gmail.com>
2020-01-01 18:59:46 -0500

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,12 @@ import {useSelector, useDispatch} from 'react-redux';
 
 const Header = () => {
   const counter = useSelector(state => state.counter)
+  const users = useSelector(state => state.products)
   return (
     <header>
       <h2>Shoppie</h2>
       <h3>Cart: {counter}</h3>
+      {console.log(users)}
     </header>
   );
 };
